0847a65364cc148981154d1eda2365f576c3026f
João Junior <joaodcpjunior@gmail.com>
2021-08-08 16:52:45 -0300

    Configurado as roles que serão atribuidas aos usuarios

---
+++ src/main/java/com/example/demo/security/ApplicationUserRole.java
@@ -0,0 +1,23 @@
+package com.example.demo.security;
+
+import java.util.Set;
+
+import com.google.common.collect.Sets;
+
+import static com.example.demo.security.ApplicationUserPermission.*;
+
+public enum ApplicationUserRole {
+    STUDENT(Sets.newHashSet()),
+    ADMIN(Sets.newHashSet(COURSE_READ, COURSE_WRITE, STUDENT_READ, STUDENT_WRITE));
+
+    private final Set<ApplicationUserPermission> permissions;
+
+    private ApplicationUserRole(Set<ApplicationUserPermission> permissions) {
+        this.permissions = permissions;
+    }
+
+    public Set<ApplicationUserPermission> getPermissions() {
+        return permissions;
+    }
+    
+}
